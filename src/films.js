d4b78d758e7e367e0e31cafbf09c3eacbffa2bc5
Isaac Malagón <isaacmalagongarcia@gmail.com>
2025-11-03 10:25:57 +0100

    ex1 done

--- src/films.js
+++ src/films.js
@@ -1,8 +1,8 @@
 // Exercise 1: Get the array of all directors.
 function getAllDirectors(array) {
-  let result =  ???;
-  console.log("EXERCICE 1 ->", result);
-  return result;
+  let directors =  movies.map(name => name.director);
+  console.log("EXERCICE 1 ->", directors);
+  return directors;
 }
 
 // Exercise 2: Get the films of a certain director
